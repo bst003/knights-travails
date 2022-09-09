a4ce837d6c10c49b2b1ecf3eb4cb8caae229b51e
bst003 <bstanner33@yahoo.com>
2022-09-08 23:06:14 -0400

    Add notes about project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import { pubsub } from "./modules/pubsub";
+
 console.log("test");
+
+/*
+
+NOTES
+
+Create Knight "node" with all possible options it could go to?
+    Could work similar to social netowk graph
+    Would have to accomodate for 8 children at points
+        Level order traversal? - https://www.youtube.com/watch?v=86g8jAQug04
+        Could get exponentially larger with each move
+    Would this lead to an infinite loop?
+    Where does game board come in on this solution?
+
+Create gameboard similar to Tic Tac Toe board?
+    How dow this accomplish getting to another spot as quickl as possible?
+*/
